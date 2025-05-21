0c8d5dc7de160de57b4360e5220aabda4c3d63d3
GitHubNexon <johnmarkpulmano052802@gmail.com>
2025-05-21 15:16:26 +0800

    fix packages

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,12 +1,12 @@
-import "dotenv/config"; // loads .env into process.env at build time
+import "dotenv/config";
 
-export default ({ config }) => {
-  return {
-    ...config,
-    extra: {
-      NEWS_API_KEY:
-        process.env.NEWS_API_KEY || "aa783906edfb425eb1f948d8c2902650",
-      API_URL: process.env.API_URL || "https://newsapi.org/v2",
-    },
-  };
-};
+export default ({ config }) => ({
+  ...config,
+  projectId: "6b1e1e30-9c21-4329-a3a8-4b8ac9dce0e8",
+  extra: {
+    ...config.extra,
+    NEWS_API_KEY:
+      process.env.NEWS_API_KEY || "aa783906edfb425eb1f948d8c2902650",
+    API_URL: process.env.API_URL || "https://newsapi.org/v2",
+  },
+});
